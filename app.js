d6527e6c9a4e61ac04ac1a9a365d26923de76d5e
jopms <joao.34@hotmail.com>
2021-01-18 22:08:27 +0000

    Added function to refresh page every 1 second

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var currentDate = Date.now() / 1000; //to give seconds
-var birthdayEsteves= 1632960000; //30 Set 
-var timeLeft = birthdayEsteves - currentDate;
 
-var secondsLeft = Math.floor(timeLeft % 60);
-var minutesLeft = Math.floor((timeLeft/60)%(60));
-var hoursLeft = Math.floor((timeLeft/60/60)%60);
-var daysLeft = Math.floor(timeLeft/60/60/24);
 
 function changeHTML () {
+    var currentDate = Date.now() / 1000; //to give seconds
+    var birthdayEsteves = 1632960000; //30 Set 
+    var timeLeft = birthdayEsteves - currentDate;
+    var secondsLeft = Math.floor(timeLeft % 60);
+    var minutesLeft = Math.floor((timeLeft/60)%(60));
+    var hoursLeft = Math.floor((timeLeft/60/60)%60);
+    var daysLeft = Math.floor(timeLeft/60/60/24);
+
     document.getElementById("days").innerHTML = daysLeft;
     document.getElementById("hours").innerHTML = hoursLeft;
     document.getElementById("minutes").innerHTML = minutesLeft;
     document.getElementById("seconds").innerHTML = secondsLeft;
 }
 
-changeHTML ();
+var refresh = setInterval(changeHTML, 1000)
